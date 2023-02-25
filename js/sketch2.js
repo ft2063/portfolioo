886ab39c1793cfa653a569b43db24254e4e5135b
Flavia Trotolo <72688128+ft2063@users.noreply.github.com>
2023-02-25 17:22:21 -0500

    Update sketch2.js

--- js/sketch2.js
+++ js/sketch2.js
@@ -74,108 +74,108 @@ var myp5 = new p5(a, 'c2');
 
 
 
-// Sketch Two
-var b = function( p ) { 
-    p.setup = function() {
-      canvas = p.createCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
-      canvas.parent('c3'); // set the canvas parent element to the div with id "c2"
-    p.angleMode(p.DEGREES)
-    p.rectMode(p.CENTER)
-  };
+// // Sketch Two
+// var b = function( p ) { 
+//     p.setup = function() {
+//       canvas = p.createCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
+//       canvas.parent('c3'); // set the canvas parent element to the div with id "c2"
+//     p.angleMode(p.DEGREES)
+//     p.rectMode(p.CENTER)
+//   };
 
-  p.draw = function() {
-    p.background(10,20,30)
-    p.noFill()
-    p.translate(p.width/2, p.height/2)
+//   p.draw = function() {
+//     p.background(10,20,30)
+//     p.noFill()
+//     p.translate(p.width/2, p.height/2)
     
-    for (var i =0; i<200; i++){
-      p.push()
+//     for (var i =0; i<200; i++){
+//       p.push()
       
-      p.rotate(p.sin(p.frameCount+i)*100)
+//       p.rotate(p.sin(p.frameCount+i)*100)
       
-      var r = p.map(p.sin(p.frameCount), -1, 1, 50, 255)
-      var b = p.map(p.sin(p.frameCount/4), -1, 1, 50, 255)
-      p.stroke(r,p.random(200,250),b)
-      p.rect(0,0,(400-i*3)/1.5,(600-i*3)/1.5, 200-i)
-      p.pop()
+//       var r = p.map(p.sin(p.frameCount), -1, 1, 50, 255)
+//       var b = p.map(p.sin(p.frameCount/4), -1, 1, 50, 255)
+//       p.stroke(r,p.random(200,250),b)
+//       p.rect(0,0,(400-i*3)/1.5,(600-i*3)/1.5, 200-i)
+//       p.pop()
       
-    }
+//     }
 
-  };
+//   };
 
-  window.addEventListener('resize', function() {
-    p.resizeCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
-  });
-};
-var myp5 = new p5(b, 'c3');
+//   window.addEventListener('resize', function() {
+//     p.resizeCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
+//   });
+// };
+// var myp5 = new p5(b, 'c3');
 
 
-// // Sketch Three
-var c = function( p ) { // p could be any variable name
-  var rotation = 0;
-  var len = 100;
-  p.setup = function() {
-    canvas = p.createCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
-    canvas.parent('c1'); // set the canvas parent element to the div with id "c2"
-    p.angleMode(p.DEGREES);
-    p.background(255)
-  };
+// // // Sketch Three
+// var c = function( p ) { // p could be any variable name
+//   var rotation = 0;
+//   var len = 100;
+//   p.setup = function() {
+//     canvas = p.createCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
+//     canvas.parent('c1'); // set the canvas parent element to the div with id "c2"
+//     p.angleMode(p.DEGREES);
+//     p.background(255)
+//   };
 
-  p.draw = function() {
-    p.background(0, 0, 0, 15);
+//   p.draw = function() {
+//     p.background(0, 0, 0, 15);
   
-    p.fill(0,255,0)
-    p.translate(p.width/2, p.height/2)
-    p.rotate(rotation)
-    p.ellipse(len,0,10)
+//     p.fill(0,255,0)
+//     p.translate(p.width/2, p.height/2)
+//     p.rotate(rotation)
+//     p.ellipse(len,0,10)
     
     
-    p.fill(0,0,255)
-    p.translate(len, 0)
-    p.rotate(rotation)
-    p.ellipse(len,0,10)
+//     p.fill(0,0,255)
+//     p.translate(len, 0)
+//     p.rotate(rotation)
+//     p.ellipse(len,0,10)
     
-    p.fill(298,0,100)
-    p.translate(len, 0)
-    p.rotate(rotation*2)
-    p.ellipse(len,0,10)
+//     p.fill(298,0,100)
+//     p.translate(len, 0)
+//     p.rotate(rotation*2)
+//     p.ellipse(len,0,10)
     
       
     
-    p.translate(len, 0)
-    p.rotate(rotation*3)
-    p.fill(255,255,0)
-    p.ellipse(len,0,10)
+//     p.translate(len, 0)
+//     p.rotate(rotation*3)
+//     p.fill(255,255,0)
+//     p.ellipse(len,0,10)
     
     
-    p.translate(len, 0)
-    p.rotate(rotation*3)
-    p.fill(255,128,100)
-    p.ellipse(len,0,10)
+//     p.translate(len, 0)
+//     p.rotate(rotation*3)
+//     p.fill(255,128,100)
+//     p.ellipse(len,0,10)
     
     
     
-    p.translate(len, 0)
-    p.rotate(rotation*3)
-    p.fill(255,0,255)
-    p.ellipse(len,0,10)
+//     p.translate(len, 0)
+//     p.rotate(rotation*3)
+//     p.fill(255,0,255)
+//     p.ellipse(len,0,10)
     
-    p.translate(len, 0)
-    p.rotate(rotation*2)
-    p.fill(51,255,255)
-    p.ellipse(len,0,10)
+//     p.translate(len, 0)
+//     p.rotate(rotation*2)
+//     p.fill(51,255,255)
+//     p.ellipse(len,0,10)
     
-    p.translate(len, 0)
-    p.rotate(rotation*2)
-    p.fill(178,255,102)
-    p.ellipse(len,0,10)
+//     p.translate(len, 0)
+//     p.rotate(rotation*2)
+//     p.fill(178,255,102)
+//     p.ellipse(len,0,10)
     
-    rotation =  rotation + 2;
-  };
+//     rotation =  rotation + 2;
+//   };
 
-  window.addEventListener('resize', function() {
-    p.resizeCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
-  });
-};
-var myp5 = new p5(c, 'c1');
+//   window.addEventListener('resize', function() {
+//     p.resizeCanvas(document.documentElement.clientWidth * 0.8, document.documentElement.clientHeight *0.8);
+//   });
+// };
+// var myp5 = new p5(c, 'c1');
 
